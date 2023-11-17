a8dc1b5773d46a381c1c43cac3cf51c259005b50
arda-guler <80536083+arda-guler@users.noreply.github.com>
2023-11-18 02:17:43 +0300

    Plot Earth orbit
    ...and minor repo organization changes

--- MPC_map.py
+++ MPC_map.py
@@ -64,7 +64,7 @@ def generate_orbits(NEOs):
             if n.orbit_type == "Atira":
                 color = "olive"
             elif n.orbit_type == "Aten":
-                color = "blue"
+                color = "cyan"
             elif n.orbit_type == "Apollo":
                 color = "red"
             elif n.orbit_type == "Amor":
@@ -84,26 +84,55 @@ def generate_orbits(NEOs):
             elif n.orbit_type == "Distant Object":
                 color = "navy"
 
-        ax.plot(x_rot, y_rot, z_rot, label='Orbit', color=color)
+        ax.plot(x_rot, y_rot, z_rot, color=color)
 
-    custom_legend = [Line2D([0], [0], color="olive", lw=2),
-                     Line2D([0], [0], color="blue", lw=2),
-                     Line2D([0], [0], color="red", lw=2),
-                     Line2D([0], [0], color="orange", lw=2),
-                     Line2D([0], [0], color="purple", lw=2),
-                     Line2D([0], [0], color="pink", lw=2),
-                     Line2D([0], [0], color="gray", lw=2),
-                     Line2D([0], [0], color="green", lw=2),
-                     Line2D([0], [0], color="brown", lw=2),
-                     Line2D([0], [0], color="aquamarine", lw=2),
-                     Line2D([0], [0], color="navy", lw=2)]
+    # -- plot Earth orbit
+    eccentricity = 0.0167086
+    argument_perihelion = np.radians(288.1)
+    semimajor_axis = 1.0000010178
+    ascending_node = np.radians(174.9)
+    inclination = 0
     
-    ax.legend(custom_legend, ['Atira', 'Aten', 'Apollo', 'Amor',
+    true_anomaly = np.linspace(0, 2 * np.pi, 1000)
+
+    r = (semimajor_axis * (1 - eccentricity**2)) / (1 + eccentricity * np.cos(true_anomaly))
+
+    x = r * np.cos(true_anomaly)
+    y = r * np.sin(true_anomaly)
+    z = np.zeros_like(true_anomaly)
+
+    x_rot = x * (np.cos(argument_perihelion) * np.cos(ascending_node) - np.sin(argument_perihelion) * np.sin(ascending_node) * np.cos(inclination)) - \
+            y * (np.sin(argument_perihelion) * np.cos(ascending_node) + np.cos(argument_perihelion) * np.sin(ascending_node) * np.cos(inclination))
+
+    y_rot = x * (np.cos(argument_perihelion) * np.sin(ascending_node) + np.sin(argument_perihelion) * np.cos(ascending_node) * np.cos(inclination)) + \
+            y * (np.cos(argument_perihelion) * np.cos(ascending_node) - np.sin(argument_perihelion) * np.sin(ascending_node) * np.cos(inclination))
+
+    z_rot = x * np.sin(argument_perihelion) * np.sin(inclination) + y * np.sin(argument_perihelion) * np.sin(inclination)
+
+    color = "blue"
+    ax.plot(x_rot, y_rot, z_rot, color=color, lw=3)
+    # -- END plot Earth orbit
+
+    custom_legend = [Line2D([0], [0], color="blue", lw=3),
+                     Line2D([0], [0], color="olive", lw=1),
+                     Line2D([0], [0], color="cyan", lw=1),
+                     Line2D([0], [0], color="red", lw=1),
+                     Line2D([0], [0], color="orange", lw=1),
+                     Line2D([0], [0], color="purple", lw=1),
+                     Line2D([0], [0], color="pink", lw=1),
+                     Line2D([0], [0], color="gray", lw=1),
+                     Line2D([0], [0], color="green", lw=1),
+                     Line2D([0], [0], color="brown", lw=1),
+                     Line2D([0], [0], color="aquamarine", lw=1),
+                     Line2D([0], [0], color="navy", lw=1)]
+    
+    ax.legend(custom_legend, ['Earth', 'Atira', 'Aten', 'Apollo', 'Amor',
                               'Obj. w/ peri. dist. < 1.665 AU',
                               'Hungaria', 'MBA', 'Phocaea',
                               'Hilda', 'Jupiter Trojan', 'Distant Object'])
     
     ax.scatter(0, 0, 0, color='yellow', label='Sol Barycenter')
+    
     ax.set_title('Orbit Plot')
     ax.set_xlabel('X (AU)')
     ax.set_ylabel('Y (AU)')
@@ -124,7 +153,7 @@ def filterNEO(n):
 
 def main():
     print("Reading JSON data...")
-    NEO_data = readJSON("nea_extended.json") # you may also load other JSONs from Minor Planet Center
+    NEO_data = readJSON("data/orbit/nea_extended.json") # you may also load other JSONs from Minor Planet Center
     MAX_OBJECTS = 100 # set max. number of orbits you want to plot so that your computer doesn't set itself on fire
 
     NEOs = []
